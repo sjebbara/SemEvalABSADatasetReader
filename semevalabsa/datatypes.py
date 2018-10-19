4090a075c3ee307f253200ca80ef23a8f7a2982a
Soufian Jebbara <s.jebbara@gmail.com>
2018-10-19 12:34:37 +0200

    running test for semeval 2014 dataset

--- semevalabsa/datatypes.py
+++ semevalabsa/datatypes.py
@@ -8,7 +8,7 @@ from semevalabsa import persist
 __author__ = 'sjebbara'
 
 
-class DatasetFormat(Enum):
+class DatasetFormat:
     SemEval2014 = "semeval2014"
     SemEval2015 = "semeval2015"
     SemEval2016 = "semeval2016"
@@ -17,17 +17,16 @@ class DatasetFormat(Enum):
 @attr.s
 class Dataset:
     reviews = attr.ib(factory=list)  # type:List["Review"]
-    dataset_format = attr.ib(default=None, type=DatasetFormat)
+    dataset_format = attr.ib(default=None, type=str)
 
-    # def __init__(self, reviews: List["Review"], dataset_format: DatasetFormat):
-    #     self.reviews = reviews  # type:  List[Review]
-    #     self.dataset_format = dataset_format
-    #
-    # def __iter__(self) -> Iterable["Review"]:
-    #     return iter(self.reviews)
-    #
-    # def __repr__(self):
-    #     return str(self.__dict__)
+    def __len__(self):
+        return len(self.reviews)
+
+    def __getitem__(self, item) -> "Review":
+        return self.reviews[item]
+
+    def __iter__(self) -> Iterable["Review"]:
+        return iter(self.reviews)
 
     def to_file(self, filepath: str, dataset_format: DatasetFormat = None):
         dataset_format = dataset_format or self.dataset_format
@@ -41,43 +40,18 @@ class Dataset:
 
 @attr.s
 class Review:
-    review_id = attr.ib(default=None, type=str)
+    id = attr.ib(default=None, type=str)
     sentences = attr.ib(factory=list)  # type:List["Sentence"]
     opinions = attr.ib(factory=list)  # type:List["Opinion"]
 
+    def __len__(self):
+        return len(self.sentences)
+
+    def __getitem__(self, item) -> "Sentence":
+        return self.sentences[item]
+
     def __iter__(self) -> Iterable["Sentence"]:
         return iter(self.sentences)
-
-    # def __init__(self, review_id: str = None, sentences: List["Sentence"] = None, opinions: List["Opinion"] = None):
-    #     self.id = review_id
-    #     self.sentences = sentences or []
-    #     self.opinions = opinions or []
-    #
-    # def __repr__(self):
-    #     return str(self.__dict__)
-    #
-    # def __str__(self):
-    #     s = u"--- Review [{}] ---".format(self.id)
-    #     s += u"\nSentences:"
-    #     for sentence in self.sentences:
-    #         s += u"\n  " + str(sentence)
-    #
-    #     if self.opinions:
-    #         s += u"\nText-Level Opinions:"
-    #         for opinion in self.opinions:
-    #             s += u"\n  " + str(opinion)
-    #     return s
-    #
-    # def __eq__(self, other):
-    #     if not isinstance(other, Review):
-    #         return False
-    #     elif self.id != other.id:
-    #         return False
-    #     elif self.id != other.id:
-    #         return False
-    #     elif self.id != other.id:
-    #         return False
-    #     return True
 
 
 @attr.s
@@ -88,28 +62,14 @@ class Sentence:
     out_of_scope = attr.ib(default=False, type=bool)
     opinions = attr.ib(factory=list)  # type:List["Opinion"]
 
+    def __len__(self):
+        return len(self.opinions)
+
+    def __getitem__(self, item) -> "Opinion":
+        return self.opinions[item]
+
     def __iter__(self) -> Iterable["Opinion"]:
         return iter(self.opinions)
-
-    # def __init__(self, review_id: str = None, sentence_id: str = None, text: str = None, out_of_scope: bool = False,
-    #              opinions: List["Opinion"] = None):
-    #     self.review_id = review_id
-    #     self.id = sentence_id
-    #     self.text = text
-    #     self.out_of_scope = out_of_scope
-    #     self.opinions = opinions or []
-    #
-    # def __repr__(self):
-    #     return str(self.__dict__)
-    #
-    # def __str__(self):
-    #     s = u"[{}]: '{}'".format(self.id, self.text)
-    #     if self.opinions:
-    #         s += u"\n  Sentence-Level Opinions:"
-    #         for o in self.opinions:
-    #             s += u"\n  " + str(o)
-    #         s += u"\n"
-    #     return s
 
 
 @attr.s
@@ -121,22 +81,3 @@ class Opinion:
     polarity = attr.ib(default=None, type=str)
     start = attr.ib(default=None, type=int)
     end = attr.ib(default=None, type=int)
-    # def __init__(self, target: str = None, category: str = None, entity: str = None, attribute: str = None, polarity: str = None, start: int = None,
-    #              end: int = None):
-    #     self.target = target
-    #     self.category = category
-    #     self.entity = entity
-    #     self.attribute = attribute
-    #     self.polarity = polarity
-    #     self.start = start
-    #     self.end = end
-
-    # def __repr__(self):
-    #     return str(self.__dict__)
-
-    # def __str__(self):
-    #     if self.target:
-    #         s = u"[{}; {}] '{}' ({}-{})".format(self.category, self.polarity, self.target, self.start, self.end)
-    #     else:
-    #         s = u"[{}; {}]".format(self.category, self.polarity)
-    #     return s
--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -3,15 +3,18 @@ from semevalabsa.datatypes import Dataset, Review, Sentence, Opinion, DatasetFor
 
 
 def test_semeval2014():
-    parsed_dataset = read_semeval2014("res/semeval2014.xml", aspect_terms=True, aspect_categories=True)
+    parsed_dataset = read_semeval2014("tests/res/semeval2014.xml", aspect_terms=True, aspect_categories=True)
 
-    r1 = Review(review_id="Review_1",
+    r1 = Review(id="Review_1",
                 sentences=[
                     Sentence(review_id="Review_1", id="1", text="This is a test sentence.", out_of_scope=False,
                              opinions=[
                                  Opinion(target="sentence", polarity="neutral", start=15, end=23),
                                  Opinion(category="TEST", polarity="negative"),
-                             ]),
+                             ])
+                ])
+    r2 = Review(id="Review_2",
+                sentences=[
                     Sentence(review_id="Review_2", id="2", text="Another sentence for testing purposes", out_of_scope=False,
                              opinions=[
                                  Opinion(target="sentence", polarity="positive", start=8, end=16),
@@ -20,6 +23,6 @@ def test_semeval2014():
                                  Opinion(category="ANOTHER", polarity="negative"),
                              ])
                 ])
-    target_dataset = Dataset([r1], dataset_format=DatasetFormat.SemEval2014)
+    target_dataset = Dataset([r1, r2], dataset_format=DatasetFormat.SemEval2014)
 
     assert parsed_dataset == target_dataset
